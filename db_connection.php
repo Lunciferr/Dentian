ed78a492b4574c0eb4d3ffe9121d02b8fcf83d19
Wei Lun <weilunnn@hotmail.com>
2022-06-23 14:31:25 +0800

    test

--- db_connection.php
+++ db_connection.php
@@ -6,12 +6,17 @@ class DB_Connect
     public function connect()
     {
         try {
+            $host = "us-cdbr-east-05.cleardb.net";
             $username = "b4505c2a49ed16";
             $password = "51e5c749";
             $active_group = 'default';
             $query_builder = TRUE;
-            
-            $conn = new PDO('mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_ce2aaaeb2f2fe3c', $username, $password);
+
+            $anotherUsername = "sql6501622";
+            $anotherPassword = "bKFKRjTEB9";
+            $anotherHost = "sql6.freemysqlhosting.net";
+
+            $conn = new PDO('mysql:host='. $host . '; dbname=heroku_ce2aaaeb2f2fe3c', $username, $password);
 
             return $conn;
         } catch (PDOException $e) {
