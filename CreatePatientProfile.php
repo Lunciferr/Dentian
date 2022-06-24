b644dba12e8ed8b378829c4b0118c0ef31f99a2c
Wei Lun <weilunnn@hotmail.com>
2022-06-24 14:54:57 +0800

    https fixed

--- CreatePatientProfile.php
+++ CreatePatientProfile.php
@@ -672,7 +672,7 @@ if (isset($_POST["addPatientProfileBtn"])) {
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
     function check() {
         $famNRIC = $("#familyNRIC").val();
