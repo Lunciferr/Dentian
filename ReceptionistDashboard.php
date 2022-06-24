9f1dc35a6d0c60c676d4e2881093f2ac5fc8f50f
Wei Lun <weilunnn@hotmail.com>
2022-06-24 14:50:05 +0800

    test

--- ReceptionistDashboard.php
+++ ReceptionistDashboard.php
@@ -182,7 +182,7 @@ if (isset($_POST["allPatients"])) {
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
     $('document').ready(function() {
         $('.editBtn').click(function() {
